1051412add7ef0f19bb6fc1f3a1fa544909c5c8e
jokeramn <seraapa@mail.ru>
2023-11-26 15:27:11 +0300

    added sign-in, sign-up, errors, edit-profile pages

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import {defineConfig, Plugin} from 'vite';
+import {resolve} from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials'),
+        }) as unknown as Plugin,
+    ],
+
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                signin: resolve(__dirname, 'src/pages/sign-in/sign-in.html'),
+                signup: resolve(__dirname, 'src/pages/sign-up/sign-up.html'),
+                404: resolve(__dirname, 'src/pages/errors/404/404-error.html'),
+                500: resolve(__dirname, 'src/pages/errors/500/500-error.html'),
+            }
+        }
+    },
+    server: {
+        open: '/src/pages/sign-in/sign-in.html'
+    }
+});
